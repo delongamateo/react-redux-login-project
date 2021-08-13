38cad8c94fb1e246b7849362ab1ae624fd0b8161
delongamateo <delongamateo@gmail.com>
2021-08-13 17:08:39 +0200

    added font,router and components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.scss';
+import Intro from './components/Intro/Intro'
+import SetUsername from './components/SetUsername/SetUsername'
+import SetPassword from './components/SetPassword/SetPassword'
+
 
 function App() {
   return (
     <div className="App">
-      
+      <Intro />
     </div>
   );
 }
---
+++ src/components/Intro/Intro.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import "./Intro.scss"
+
+function Intro() {
+    return (
+        <>
+            <div className="logoName">
+
+            </div>
+            <div className="iconText">
+
+            </div>
+            <div className="form">
+
+            </div>
+            <div className="button">
+                
+            </div>
+        </>
+    )
+}
+
+export default Intro
