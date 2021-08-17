c71b64bf644ba5bc9eaa482d33a5274607c721ea
delongamateo <delongamateo@gmail.com>
2021-08-18 00:23:09 +0200

    update reducers done

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,14 +1,22 @@
 import React, {useState, useEffect} from 'react'
 import "../scss/profile.scss"
 import { useSelector, useDispatch } from 'react-redux'
-import { updateCity } from '../../features/user/userSlice'
+import { updateName, updateSurname, updateUsername, updateAddress, updateAddressNumber, updateCity, updatePostalCode, updateEmail, updatePhone, updateSocialMedia } from '../../features/user/userSlice'
 import { Link } from "react-router-dom";
 
 
 function Profile() {
     const { user } = useSelector(state => state.user)
+    const dispatch = useDispatch();
+
+    const [name, setName] = useState("")
+    const [surname, setSurname] = useState("")
+    const [showNameInput, setShowNameInput] = useState(false)
     const [username, setUsername] = useState("")
     const [showUsernameInput, setShowUsernameInput] = useState(false)
+    const [address, setAddress] = useState("")
+    const [addressNumber, setAddressNumber] = useState("")
+    const [showAddressInput, setShowAddressInput] = useState(false)
     const [city, setCity] = useState("")
     const [showCityInput, setShowCityInput] = useState(false)
     const [postalCode, setPostalCode] = useState("")
@@ -17,11 +25,38 @@ function Profile() {
     const [showEmailInput, setShowEmailInput] = useState(false)
     const [phone, setPhone] = useState("")
     const [showPhoneInput, setShowPhoneInput] = useState(false)
+    const [socialMedia, setSocialMedia] = useState("")
+    const [showSocialMediaInput, setShowSocialMediaInput] = useState(false)
     
-    const dispatch = useDispatch();
-
+    const changeName = () => {
+        dispatch(updateName(name))
+    }
+    const changeSurname = () => {
+        dispatch(updateSurname(surname))
+    }
+    const changeUsername = () => {
+        dispatch(updateUsername(username))
+    }
+    const changeAddress = () => {
+        dispatch(updateAddress(address))
+    }
+    const changeAddressNumber = () => {
+        dispatch(updateAddressNumber(addressNumber))
+    }
     const changeCity = () => {
         dispatch(updateCity(city))
+    }
+    const changePostalCode = () => {
+        dispatch(updatePostalCode(postalCode))
+    }
+    const changeEmail = () => {
+        dispatch(updateEmail(email))
+    }
+    const changePhone = () => {
+        dispatch(updatePhone(phone))
+    }
+    const changeSocialMedia = () => {
+        dispatch(updateSocialMedia(socialMedia))
     }
 
     return (
@@ -36,32 +71,68 @@ function Profile() {
             </div>
             <div className="userInfo">
                 <div className="infoCard">
-                    <div className="info">
-                        <p className="title">Name</p>
-                        <h4 className="infoData">{`${user?.user?.name} ${user?.user?.surname}`}</h4>
-                    </div>
-                    <div className="actions">
-                        <p className="edit">Edit</p>
-                    </div>
+                    {!showNameInput &&
+                        <>
+                        <div className="info">
+                            <p className="title">Name</p>
+                            <h4 className="infoData">{`${user?.user?.name} ${user?.user?.surname}`}</h4>
+                        </div>
+                        <div className="actions">
+                            <button onClick={() => setShowNameInput(true)} className="edit">Edit</button>
+                        </div>
+                        </>
+                    }
+                    {showNameInput &&
+                        <div className="changeData">
+                            <input type="text" onChange={e => setName(e.target.value)} className="changeDataInput" value={name} />
+                            <input type="text" onChange={e => setSurname(e.target.value)} className="changeDataInput" value={surname} />
+                            <button onClick={() => setShowUsernameInput(false)} className="changeDataAction" ><img src="/assets/Cross@1x.png" /></button>
+                            <button onClick={() => { changeName(); changeSurname(); setShowNameInput(false) }} className="changeDataAction" ><img src="/assets/Next Blue@1x.png" /></button>
+                        </div>
+                    }
                 </div>
                 <div className="infoCard">
-                    <div className="info">
-                        <p className="title">Username</p>
-                        <h4 className="infoData">{user?.user?.displayName}</h4>
-                    </div>
-                    <div className="actions">
-                        <p className="edit">Edit</p>
-                    </div>
+                    {!showUsernameInput &&
+                        <>
+                            <div className="info">
+                                <p className="title">Username</p>
+                                <h4 className="infoData">{user?.user?.displayName}</h4>
+                            </div>
+                            <div className="actions">
+                                <button onClick={() => setShowUsernameInput(true)} className="edit">Edit</button>
+                                <p className="delete">Delete</p>
+                            </div>
+                        </> 
+                    }
+                    {showUsernameInput &&
+                        <div className="changeData">
+                            <input type="text" onChange={e => setUsername(e.target.value)} className="changeDataInput" value={username} />
+                            <button onClick={() => setShowUsernameInput(false)} className="changeDataAction" ><img src="/assets/Cross@1x.png" /></button>
+                            <button onClick={() => { changeUsername(); setShowUsernameInput(false) }} className="changeDataAction" ><img src="/assets/Next Blue@1x.png" /></button>
+                        </div>
+                    }
                 </div>
                 <div className="infoCard">
-                    <div className="info">
-                        <p className="title">Address</p>
-                        <h4 className="infoData">{`${user?.user?.contact?.locations[0]?.address.streetName} ${user?.user?.contact?.locations[0]?.address.streetNumber}`}</h4>
-                    </div>
-                    <div className="actions">
-                        <p className="edit">Edit</p>
-                        <p className="delete">Delete</p>
-                    </div>
+                    {!showAddressInput &&
+                        <>
+                        <div className="info">
+                            <p className="title">Address</p>
+                            <h4 className="infoData">{`${user?.user?.contact?.locations[0]?.address.streetName} ${user?.user?.contact?.locations[0]?.address.streetNumber}`}</h4>
+                        </div>
+                        <div className="actions">
+                            <button onClick={() => setShowAddressInput(true)} className="edit">Edit</button>
+                            <p className="delete">Delete</p>
+                        </div>
+                        </>
+                    }
+                    {showAddressInput &&
+                        <div className="changeData">
+                            <input type="text" onChange={e => setAddress(e.target.value)} className="changeDataInput" value={address} />
+                            <input type="text" onChange={e => setAddressNumber(e.target.value)} className="changeDataInput" value={addressNumber} />
+                            <button onClick={() => setShowAddressInput(false)} className="changeDataAction" ><img src="/assets/Cross@1x.png" /></button>
+                            <button onClick={() => { changeAddress(); changeAddressNumber(); setShowAddressInput(false) }} className="changeDataAction" ><img src="/assets/Next Blue@1x.png" /></button>
+                        </div>
+                    }
                 </div>
                 <div className="infoCard">
                     {!showCityInput &&
@@ -85,44 +156,88 @@ function Profile() {
                     }
                 </div>
                 <div className="infoCard">
-                    <div className="info">
-                        <p className="title">Postal Code</p>
-                        <h4 className="infoData">{user?.user?.contact?.locations[0]?.address?.postalCode}</h4>
-                    </div>
-                    <div className="actions">
-                        <p className="edit">Edit</p>
-                        <p className="delete">Delete</p>
-                    </div>
+                    {!showPostalCodeInput &&
+                        <>
+                            <div className="info">
+                                <p className="title">Postal Code</p>
+                                <h4 className="infoData">{user?.user?.contact?.locations[0]?.address?.postalCode}</h4>
+                            </div>
+                            <div className="actions">
+                                <button onClick={() => setShowPostalCodeInput(true)} className="edit">Edit</button>
+                                <p className="delete">Delete</p>
+                            </div>
+                        </>
+                    }
+                    {showPostalCodeInput &&
+                        <div className="changeData">
+                            <input type="text" onChange={e => setPostalCode(e.target.value)} className="changeDataInput" value={postalCode} />
+                            <button onClick={() => setShowPostalCodeInput(false)} className="changeDataAction" ><img src="/assets/Cross@1x.png" /></button>
+                            <button onClick={() => { changePostalCode(); setShowPostalCodeInput(false) }} className="changeDataAction" ><img src="/assets/Next Blue@1x.png" /></button>
+                        </div>
+                    }
                 </div>
                 <div className="infoCard">
-                    <div className="info">
-                        <p className="title">E-mail</p>
-                        <h4 className="infoData">{user?.user?.contact?.email}</h4>
-                    </div>
-                    <div className="actions">
-                        <p className="edit">Edit</p>
-                        <p className="delete">Delete</p>
-                    </div>
+                    {!showEmailInput &&
+                        <>
+                            <div className="info">
+                                <p className="title">Email</p>
+                                <h4 className="infoData">{user?.user?.contact?.email}</h4>
+                            </div>
+                            <div className="actions">
+                                <button onClick={() => setShowEmailInput(true)} className="edit">Edit</button>
+                                <p className="delete">Delete</p>
+                            </div>
+                        </>
+                    }
+                    {showEmailInput &&
+                        <div className="changeData">
+                            <input type="text" onChange={e => setEmail(e.target.value)} className="changeDataInput" value={email} />
+                            <button onClick={() => setShowEmailInput(false)} className="changeDataAction" ><img src="/assets/Cross@1x.png" /></button>
+                            <button onClick={() => { changeEmail(); setShowEmailInput(false) }} className="changeDataAction" ><img src="/assets/Next Blue@1x.png" /></button>
+                        </div>
+                    }
                 </div>
                 <div className="infoCard">
-                    <div className="info">
-                        <p className="title">Phone</p>
-                        <h4 className="infoData">{`+420 ${user?.user?.contact?.phoneNumber}`}</h4>
-                    </div>
-                    <div className="actions">
-                        <p className="edit">Edit</p>
-                        <p className="delete">Delete</p>
-                    </div>
+                    {!showPhoneInput &&
+                        <>
+                            <div className="info">
+                                <p className="title">Phone</p>
+                                <h4 className="infoData">{`+420 ${user?.user?.contact?.phoneNumber}`}</h4>
+                            </div>
+                            <div className="actions">
+                                <button onClick={() => setShowPhoneInput(true)} className="edit">Edit</button>
+                                <p className="delete">Delete</p>
+                            </div>
+                        </>
+                    }
+                    {showPhoneInput &&
+                        <div className="changeData">
+                            <input type="text" onChange={e => setPhone(e.target.value)} className="changeDataInput" value={phone} />
+                            <button onClick={() => setShowPhoneInput(false)} className="changeDataAction" ><img src="/assets/Cross@1x.png" /></button>
+                            <button onClick={() => { changePhone(); setShowPhoneInput(false) }} className="changeDataAction" ><img src="/assets/Next Blue@1x.png" /></button>
+                        </div>
+                    }
                 </div>
                 <div className="infoCard">
-                    <div className="info">
-                        <p className="title">Social Media</p>
-                        <h4 className="infoData">{user?.user?.contact?.socialNetworks[0]?.name}</h4>
-                    </div>
-                    <div className="actions">
-                        <p className="edit">Edit</p>
-                        <p className="delete">Delete</p>
-                    </div>
+                    {!showSocialMediaInput &&
+                        <>
+                            <div className="info">
+                                <p className="title">Social Media</p>
+                            <h4 className="infoData">{user?.user?.contact?.socialNetworks[0]?.name}</h4>
+                            </div>
+                            <div className="actions">
+                                <button onClick={() => setShowSocialMediaInput(true)} className="edit">Edit</button>
+                                <p className="delete">Delete</p>
+                            </div>
+                        </>
+                    }
+                    {showSocialMediaInput &&
+                        <div className="changeData">
+                            <input type="text" onChange={e => setSocialMedia(e.target.value)} className="changeDataInput" value={socialMedia} />
+                            <button onClick={() => setShowSocialMediaInput(false)} className="changeDataAction" ><img src="/assets/Cross@1x.png" /></button>
+                            <button onClick={() => { changeSocialMedia(); setShowSocialMediaInput(false) }} className="changeDataAction" ><img src="/assets/Next Blue@1x.png" /></button>
+                        </div>
+                    }
                 </div>
             </div>
             <div className="logoutContainer">
--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -18,10 +18,36 @@ const userSlice = createSlice({
     name: "user",
     initialState,
     reducers: {
+        updateName: (state, action) => {
+            state.user.user.name = action.payload
+        },
+        updateSurname: (state, action) => {
+            state.user.user.surname = action.payload
+        },
+        updateUsername: (state, action) => {
+            state.user.user.displayName = action.payload
+        },
+        updateAddress: (state, action) => {
+            state.user.user.contact.locations[0].address.streetName = action.payload
+        },
+        updateAddressNumber: (state, action) => {
+            state.user.user.contact.locations[0].address.streetNumber = action.payload
+        },
         updateCity: (state, action) => {
             state.user.user.contact.locations[0].address.suburb = action.payload
         },
-
+        updatePostalCode: (state, action) => {
+            state.user.user.contact.locations[0].address.postalCode = action.payload
+        },
+        updateEmail: (state, action) => {
+            state.user.user.contact.email = action.payload
+        },
+        updatePhone: (state, action) => {
+            state.user.user.contact.phoneNumber = action.payload
+        },
+        updateSocialMedia: (state, action) => {
+            state.user.user.contact.socialNetworks[0].name = action.payload
+        }
     },
     extraReducers: {
             [fetchUser.pending]: (state, action) => {
@@ -40,5 +66,5 @@ const userSlice = createSlice({
 });
 
 
-export const {updateCity, update} = userSlice.actions
+export const {updateName, updateSurname, updateUsername, updateAddress, updateAddressNumber, updateCity, updatePostalCode, updateEmail, updatePhone, updateSocialMedia} = userSlice.actions
 export default userSlice.reducer
